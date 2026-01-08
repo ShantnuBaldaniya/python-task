5fef0cd396828d0c346ca8f481e7c7cc3bdff9a2
shantnu <shantnubaldaniya9@gmail.com>
2026-01-08 13:29:14 +0530

    Add files via upload

---
+++ oops.py
@@ -0,0 +1,348 @@
+
+
+"""Create a base class Shape with methods to calculate area and perimeter. 
+	Derive classes Circle and Rectangle from it and override the methods"""
+
+class Shape:
+    def area(self):
+        return 0
+
+    def perimeter(self):
+        return 0
+
+
+class Circle(Shape):
+    def area(self, radius):
+        return 3.14 * radius * radius
+
+    def perimeter(self, radius):
+        return 2 * 3.14 * radius
+
+
+class Rectangle(Shape):
+    def area(self, height, width):
+        return height * width
+
+    def perimeter(self, height, width):
+        return 2 * (height + width)
+
+
+"""Create a Calculator class with multiple methods for addition, subtraction, multiplication, and division. 
+Implement method overloading to handle different parameter types (int, float, and possibly more). Demonstrate the usage of these overloaded methods"""
+
+
+class Calculator:
+   
+    def add(self, a, b):
+        return a + b
+
+    
+    def subtract(self, a, b):
+        return a - b
+
+    
+    def multiply(self, a, b):
+        return a * b
+
+    
+    def divide(self, a, b):
+        if b == 0:
+            return "Error: Division by zero"
+        return a / b
+
+
+calc = Calculator()
+
+
+print(calc.add(5, 3))          # 8
+print(calc.subtract(10, 4))    # 6
+
+# Floats
+print(calc.multiply(2.5, 4.0)) # 10.0
+print(calc.divide(7.5, 2.5))   # 3.0
+
+
+print(calc.add(5, 2.5))        # 7.5
+
+
+
+"""Implement a base class Shape with methods for calculating area and perimeter. Create derived classes such as Circle, Rectangle, 
+and Triangle that inherit from Shape and override the relevant methods. Demonstrate polymorphism by creating instances of these classes and calling the area and perimeter methods."""
+
+
+
+class Shape:
+    def area(self):
+        pass
+
+    def perimeter(self):
+        pass
+
+
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return 3.14 * self.radius * self.radius
+
+    def perimeter(self):
+        return 2 * 3.14 * self.radius
+
+
+
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width
+
+    def perimeter(self):
+        return 2 * (self.length + self.width)
+
+
+class Triangle(Shape):
+    def __init__(self, a, b, c):
+        self.a = a
+        self.b = b
+        self.c = c
+
+    def area(self):
+        return "Area"
+
+    def perimeter(self):
+        return self.a + self.b + self.c
+
+
+
+c = Circle(5)
+r = Rectangle(4, 6)
+t = Triangle(3, 4, 5)
+
+shapes = [c, r, t]
+
+for s in shapes:
+    print("Area:", s.area())
+    print("Perimeter:", s.perimeter())
+    
+
+
+"""Create a function longest_palindromic_subsequence(s: str) -> int that finds 
+the length of the longest palindromic subsequence in a given string."""
+
+def longest_palindromic_subsequence(s):
+    def lps(i, j):
+        
+        if i > j:
+            return 0
+        if i == j:
+            return 1
+
+        
+        if s[i] == s[j]:
+            return 2 + lps(i+1, j-1)
+        else:
+            return max(lps(i+1, j), lps(i, j-1))
+
+    return lps(0, len(s)-1)
+print(longest_palindromic_subsequence("bbbbabaabbab")) 
+print(longest_palindromic_subsequence("cbbd"))   
+
+
+
+"""Create a function edit_distance(word1: str, word2: str) -> int that calculates the minimum number of operations (insertion, deletion, or substitution)
+required to convert one word into another."""
+
+word1='cat'
+word2='cut'
+m = len(word1)
+n=len(word2)
+mat = []
+
+for i in range(m + 1):
+    row = []
+    for j in range(n + 1):
+        row.append(0)
+    mat.append(row)
+
+for i in range(m + 1):
+    mat[i][0] = i
+
+
+for j in range(n + 1):
+    mat[0][j] = j
+
+
+for i in range(1, m + 1):
+    for j in range(1, n + 1):
+        if word1[i - 1] == word2[j - 1]:
+            mat[i][j] = mat[i - 1][j - 1]   
+            
+        else:
+            mat[i][j] = 1 + min(
+                mat[i - 1][j],     
+                mat[i][j - 1],   
+                mat[i - 1][j - 1]  
+            )
+print(mat)
+
+
+
+
+"""Define a function can_partition(nums: List[int], target_sum: int) -> bool that determines whether it is possible to partition a given set of numbers
+into two subsets such that the sum of elements in both subsets is equal."""
+
+def can_partition(nums) :
+    total = sum(nums)
+
+
+    if total % 2 != 0:
+        return False
+
+    target = total // 2
+    possible = {0}   
+
+    for num in nums:
+        new_sums = set()
+        for s in possible:
+            new_sums.add(s + num)
+        possible |= new_sums
+
+    return target in possible
+result=can_partition([1,2,3,6])
+print(result)
+
+
+def number(n):
+    if n < 0:
+        print('Enter a positive number')
+        return
+    if n == 0:       
+        return
+    number(n-1)       
+    print(n)         
+
+num = int(input('Enter the range = '))
+number(num)
+
+
+def factorial(num):
+    if num==0:
+        return 1
+    return num * factorial(num-1)
+
+
+    
+def sum(list):
+    if len(list)==0:
+        return 0
+    return list[0]+sum(list[1:])
+list=[1,2,3,4,5,6]
+print(sum(list))
+
+
+
+def count_list(lst):
+    if len(lst) == 0:     
+        return 0
+    return 1 + count_list(lst[1:])  
+
+lst = [1, 2, 3, 4, 5]
+print(count_list(lst))
+
+
+
+
+def palin(string):
+    if len(string)>=1:
+        return True
+    if string[0]!=string[-1]:
+        return False
+    return palin(string[1:-1])
+
+
+   
+def game(user):
+    if user == 0:        
+        return
+
+    game(user-1)           
+
+    if user % 3 == 0 and user % 5 == 0:
+        print('fizzbuzz')
+    elif user % 3 == 0:
+        print('fizz')
+    elif user % 5 == 0:
+        print('buzz')
+    else:
+        print(user)
+
+number = int(input('enter the number = '))
+game(number)
+
+
+def leap_year(year):
+    if year <= 0:    
+        return False
+    
+    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+        return True
+    else:
+        return False
+
+year = int(input("Enter a year: "))
+print("Leap year" if leap_year(year) else "Not a leap year")
+
+
+import random
+
+
+
+
+def game_start():
+    user=None
+    import random
+    number=random.randint(1,100)
+    attempt=0
+    print(number)
+    
+    while number!=user:
+        attempt+=1
+        user=int(input('enter the guess='))
+        if number>user:
+            print('make a gues icrease')
+        elif number<user:
+            print('less your guess')
+        else :
+            print(f'u win the game in {attempt} attempt ')
+            break
+        
+    
+def op(list):
+    max=0
+    rd=[]
+    sorting=[]
+    for i in list:
+        if i>=max:
+            max=i
+    print('the max number is=',max)
+    
+    for i in list:
+        if i not in rd:
+            rd.append(i)
+    
+    
+            
+    print('after a remove duplicate',rd)
+list=[1,1,1,1,2,3,4,4,4,4,4,4,4,4,4,4,5,5,65,6]
+op(list)  
+
+def digits(number):
+    if number==0:
+        return 0
+    return number%10+digits(number//10)
+result=digits(3232323)
+print(result)     
---
+++ recursion.py
@@ -0,0 +1,112 @@
+
+#Calculate the factorial of a number using recursion.
+
+def factorial(num):
+    if num==0:
+        return 1
+    return num * factorial(num-1)
+
+
+#Calculate the sum of all elements in a list using recursion.
+   
+def sum(list):
+    if len(list)==0:
+        return 0
+    return list[0]+sum(list[1:])
+list=[1,2,3,4,5,6]
+print(sum(list))
+
+
+
+#Count the number of elements in a list using recursion
+
+def count_list(lst):
+    if len(lst) == 0:     
+        return 0
+    return 1 + count_list(lst[1:])  
+
+lst = [1, 2, 3, 4, 5]
+print(count_list(lst))
+
+
+
+
+
+#Write a function that checks if a given string is a palindrome (reads the same forwards and backward), ignoring spaces and letter casing.
+
+def palin(string):
+    if len(string)>=1:
+        return True
+    if string[0]!=string[-1]:
+        return False
+    return palin(string[1:-1])
+
+
+ #Implement the FizzBuzz game: Print numbers from 1 to n, but for multiples of 3, print "Fizz," for multiples of 5, print "Buzz," and for multiples of both 3 and 5, print "FizzBuzz."
+  
+def game(user):
+    if user == 0:        
+        return
+    game(user-1)           
+    if user % 3 == 0 and user % 5 == 0:
+        print('fizzbuzz')
+    elif user % 3 == 0:
+        print('fizz')
+    elif user % 5 == 0:
+        print('buzz')
+    else:
+        print(user)
+
+number = int(input('enter the number = '))
+game(number)
+
+
+
+#Implement a function that checks if a given year is a leap year
+
+def leap_year(year):
+    if year <= 0:    
+        return False
+    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+        return True
+    else:
+        return False
+
+year = int(input("Enter a year: "))
+print("Leap year" if leap_year(year) else "Not a leap year")
+
+
+
+
+#Create a simple number guessing game where the program generates a random number, and the user has to guess it
+
+import random
+def game_start():
+    user=None
+    import random
+    number=random.randint(1,100)
+    attempt=0
+    print(number)
+    
+    while number!=user:
+        attempt+=1
+        user=int(input('enter the guess='))
+        if number>user:
+            print('make a gues icrease')
+        elif number<user:
+            print('less your guess')
+        else :
+            print(f'u win the game in {attempt} attempt ')
+            break
+        
+
+
+#Implement a function that calculates the sum of the digits of a positive integer. 
+
+
+def digits(number):
+    if number==0:
+        return 0
+    return number%10+digits(number//10)
+result=digits(3232323)
+
